884e0fe791a1780660d456ee50dfad972dede0b9
aramaramyan <aramaramyan1997@mail.ru>
2022-07-28 15:17:55 +0400

    feta: add pending cases

--- src/store/appSlice.js
+++ src/store/appSlice.js
@@ -16,15 +16,24 @@ const appSlice = createSlice({
   initialState: {
     restaurants: [],
     current: {},
-    isLoadingRestaurants: false
+    isLoadingRestaurants: false,
+    isLoadingRestaurant: false
   },
   reducers: {},
   extraReducers: (builder) => {
+    builder.addCase(getRestaurantsThunk.pending, state => {
+      state.isLoadingRestaurants = true;
+    });
     builder.addCase(getRestaurantsThunk.fulfilled, (state, action) => {
       state.restaurants = action.payload;
+      state.isLoadingRestaurants = false;
+    });
+    builder.addCase(getRestaurantThunk.pending, state => {
+      state.isLoadingRestaurant = true;
     });
     builder.addCase(getRestaurantThunk.fulfilled, (state, action) => {
       state.current = action.payload;
+      state.isLoadingRestaurant = false;
     });
   }
 });
